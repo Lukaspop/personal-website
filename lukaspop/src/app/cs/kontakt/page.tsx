6f057f535a59cf7e73b1e8440a12ed58e793569a
Lukas <Luk4sp0p@gmail.com>
2026-03-08 20:20:30 +0100

    EFN

--- lukaspop/src/app/cs/kontakt/page.tsx
+++ lukaspop/src/app/cs/kontakt/page.tsx
@@ -2,18 +2,7 @@ export default function Kontakt() {
   return (
     <div>
       <h1>Kontakt – czech</h1>
-      <p>123</p>
-      <p>123</p>
-      <p>123</p>
-      <p>123</p>
-      <p>123</p>
-      <p>123</p>
-      <p>Ras dva tri</p>
-      <p>Ras dva tri</p>
-      <p>Ras dva tri</p>
-      <p>Ras dva tri</p>
-      <p>Ras dva tri</p>
-      <p>Ras dva tri</p>
+      <p>miluju gympl</p>
     </div>
   );
 }
